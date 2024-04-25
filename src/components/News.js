18869463eb133de0d5129f1b8ad8f4c456d756f6
Shivam Soni <shivamsoni8445@gmail.com>
2024-04-25 18:20:13 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -2,55 +2,103 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
+    // articles = [
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+    //         "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+    //         "publishedAt": "2020-04-27T11:41:47Z",
+    //         "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+    //         "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+    //         "publishedAt": "2020-03-30T15:26:05Z",
+    //         "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    //     }
+    // ]
     constructor() {
         super();
         // console.log("Hello I am a constructor from News component");
         this.state = {
-            articles: this.articles,
-            loading: false
+            // articles: this.articles,
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+
+    async componentDidMount() {
+        // console.log("componentDidMount");
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=10c6936ab4b94e038ebddffe7b7e483d&pageSize=20";
+        let data = await fetch(url);
+        let parseData = await data.json();
+        console.log(parseData);
+        this.setState({
+            articles: parseData.articles,
+            totalResults: parseData.totalResults
+        })
+    }
+
+    handlePrevClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=10c6936ab4b94e038ebddffe7b7e483d&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        console.log(parseData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parseData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("Next");
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=10c6936ab4b94e038ebddffe7b7e483d&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parseData = await data.json();
+            console.log(parseData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parseData.articles
+            })
         }
     }
 
     render() {
+        // console.log("render");
         return (
             <div className='container my-3'>
                 {/* This is a news component */}
-                <h2>NewsMonkey - Top Headlines</h2>
+                <h1>NewsMonkey - Top Headlines</h1>
                 <div className="row">
                     {/* {this.state.articles.map((element) => { console.log(element) })} */}
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div >
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2024/04/24/1600x900/garena_free_fire_max_1706148751443_1713932203141.jpeg":imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
